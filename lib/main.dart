f5c65af1dc2807c92e6be0cae6758dc8e3566b94
MR GREEN AGI <pandey.abhinav87@gmail.com>
2025-08-13 16:33:31 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tutor App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tutor App - Preschool to Class 5'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to the Tutor App!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
